a79c940cb870a572174103c66a79f37cd557131d
Psi <xiepengxi@gmail.com>
2011-05-30 21:13:57 +0800

    init

---
+++ install_redis.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Set up the environment. Respect $VERSION if it's set.
+    set -e
+    REDIS_ROOT="/opt/redis"
+    REDIS_ROOT_BIN="/opt/redis/bin/redis-server"
+    REDIS_BIN="/usr/bin/redis-server"
+    REDIS_CONF_ROOT="/etc/redis"
+    [[ -z "$VERSION" ]] && VERSION=2.2.8
+
+    echo "Setup Redis $VERSION"
+
+# Create the Redis directory structure if it doesn't already exist.
+    mkdir -p "$REDIS_ROOT/versions" "$REDIS_ROOT/bin" "$REDIS_CONF_ROOT"
+
+# If the requested version of Redis is already installed, remove it first.
+
+    cd "$REDIS_ROOT/versions"
+    rm -rf "$REDIS_ROOT/versions/$VERSION"
+# Download the requested version of Redis and unpack it.
+    curl -s http://redis.googlecode.com/files/redis-$VERSION.tar.gz | tar xzf -
+    mv "redis-$VERSION" "$VERSION"
+    cd "$REDIS_ROOT/versions/$VERSION"
+    make
+    rm -f "$REDIS_ROOT_BIN"
+    ln -s "$REDIS_ROOT/versions/$VERSION/src/redis-server" "$REDIS_ROOT_BIN"
+    rm -f "$REDIS_BIN"
+    ln -s "$REDIS_ROOT_BIN" "$REDIS_BIN"
+
+    curl -L https://github.com/Psli/coo/raw/master/redis.conf > "$REDIS_CONF_ROOT/redis.conf"
+    curl -L https://github.com/Psli/coo/raw/master/redis_server > /etc/init.d/redis_server
+    chmod u+x /etc/init.d/redis_server
+
+    sudo /sbin/chkconfig --add redis-server
+    sudo /sbin/chkconfig --level 345 redis-server on
+    sudo /sbin/service redis-server start
+    echo "OK!!"
+
